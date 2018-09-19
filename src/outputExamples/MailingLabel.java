6f86662d9721d9c860bcc4c93f67fbafbd985435
Aaron Melcher <amelch130@gmail.com>
2018-09-19 07:51:09 -0500

    Updated examples/minimal work

--- src/outputExamples/MailingLabel.java
+++ src/outputExamples/MailingLabel.java
@@ -4,7 +4,7 @@ public class MailingLabel {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
- System.out.println("Aaron Melcher\n12345 Medical Center\nBrookfield WI 53045");
+		System.out.println("Aaron Melcher\n12345 Medical Center\nBrookfield WI 53045");
 	}
 
 }

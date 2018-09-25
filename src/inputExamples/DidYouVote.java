0b69f80f0a580cf083a04a88f72d63071e7c056f
Aaron Melcher <amelch130@gmail.com>
2018-09-25 08:20:00 -0500

    Updated Vote

--- src/inputExamples/DidYouVote.java
+++ src/inputExamples/DidYouVote.java
@@ -7,10 +7,10 @@ public class DidYouVote {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		boolean vote = false;
-Scanner input = new Scanner(System.in);
-System.out.println("Did you vote?");
-vote = input.nextBoolean();
-System.out.println("Voting status: "+vote);
+		Scanner input = new Scanner(System.in);
+		System.out.println("Did you vote?");
+		vote = input.nextBoolean();
+		System.out.println("Voting status: " + vote);
 	}
 
 }

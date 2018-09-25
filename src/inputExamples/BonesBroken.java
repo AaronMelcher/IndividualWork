b84bb0f52b7ffd5ce10862150bb35e985e0a3173
Aaron Melcher <amelch130@gmail.com>
2018-09-25 08:19:46 -0500

    New Input test

---
+++ src/inputExamples/BonesBroken.java
@@ -0,0 +1,17 @@
+package inputExamples;
+
+import java.util.Scanner;
+
+public class BonesBroken {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		int brokenBones = -999;
+		System.out.println("How many bones have you broken?");
+		brokenBones = input.nextInt();
+		System.out.println("You have broken " + brokenBones + " bones.");
+
+	}
+
+}

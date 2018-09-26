59fccef7d907d15d5158547f2d5a329a1e714f33
Aaron Melcher <amelch130@gmail.com>
2018-09-26 08:01:18 -0500

    Update to Pass Generator

--- src/inputExamples/PasswordGenerator.java
+++ src/inputExamples/PasswordGenerator.java
@@ -1,10 +1,21 @@
 package inputExamples;
-
+import java.util.Scanner;
 public class PasswordGenerator {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int first = -999;
+		int second = -999;
+		int third = -999;
+		Scanner input = new Scanner(System.in);
+		System.out.println("Type in a one digit number:");
+		first = input.nextByte();
+		System.out.println("Type in another one digit number:");
+		second = input.nextByte();
+		System.out.println("Type in a final one digit number:");
+		third = input.nextByte();
+		System.out.println("All possible combinations are:\n");
+		input.close();
 	}
 
 }

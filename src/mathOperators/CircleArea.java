5631909596610567799d326e456a7c03c2b70b31
Aaron Melcher <amelch130@gmail.com>
2018-09-27 19:09:58 -0500

    Fixed issue in Circle Activity

--- src/mathOperators/CircleArea.java
+++ src/mathOperators/CircleArea.java
@@ -6,7 +6,6 @@ public class CircleArea {
 		// TODO Auto-generated method stub
 		Scanner input = new Scanner(System.in);
 		double radius = -999.0;
-		final double PI = 3.14;
 		double area = -999;
 		System.out.println("What is the radius of the circle?");
 		radius = input.nextDouble();

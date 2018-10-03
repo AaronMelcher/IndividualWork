5fb68cf4c26211e7c859b6501b8bced9537907ac
Aaron Melcher <amelch130@gmail.com>
2018-10-02 20:40:37 -0500

    Final Cutting Grass 2

--- src/mathOperators/CuttingGrass2.java
+++ src/mathOperators/CuttingGrass2.java
@@ -7,28 +7,33 @@ public class CuttingGrass2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner input = new Scanner(System.in);
-		double lenght;
+		double length;
 		double width;
 		double mph;
 		double deck;
-		double area;
+		double passes;
+		double timePass;
 		double time;
-		double totPay;
-		double grassCut;
+		double pay;
 		final int RATE = 12;
 		System.out.println("What is the legnth of the lawn (in feet)?");
-		lenght = input.nextDouble();
+		length = input.nextDouble();
 		System.out.println("What is the width of the lawn (in feet)?");
 		width = input.nextDouble();
 		System.out.println("What is the size of the mower deck (in inches)?");
 		deck = input.nextDouble();
 		System.out.println("What is the speed of the mower (in MPH)?");
 		mph = input.nextDouble();
-		area = (lenght*width);
-		grassCut = deck*mph;
-		time = area/grassCut;
-		totPay = time*RATE;
-		System.out.println("It will take "+time+" hours to cut the grass \n and the person will be owed $"+totPay);
+		length = length*12;
+		passes = length/deck;
+		width = width/5280;
+		timePass = width/mph;
+		time = timePass * passes;
+		pay = time * RATE;
+		System.out.println("It will take "+time+" hours to mow the lawn,\nand the person mowing the lawn is owed $"+pay);
+		
+		
+		
 		input.close();
 	}
 

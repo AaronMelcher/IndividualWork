0e727214cdc72b55662d81e6aa020297a0bec49d
Aaron Melcher <amelch130@gmail.com>
2018-10-11 08:53:51 -0500

    Numbers Class and Parameters

---
+++ src/objectOrientedProgramming/Numbers.java
@@ -0,0 +1,20 @@
+package objectOrientedProgramming;
+
+public class Numbers {
+	public Numbers() {
+
+	}
+
+	public void sayNumber(int num) {
+		System.out.println(num);
+	}
+
+	public void sayNumberPlus2(int num) {
+		System.out.println(num + 2);
+	}
+
+	public void saySum(int num, int num2) {
+		int result = num + num2;
+		System.out.println(result);
+	}
+}
---
+++ src/objectOrientedProgramming/TestNumbers.java
@@ -0,0 +1,13 @@
+package objectOrientedProgramming;
+
+public class TestNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Numbers bob = new Numbers();
+		bob.sayNumber(3);
+		bob.sayNumberPlus2(3);
+		bob.saySum(3,4);
+	}
+
+}

b514cb423d20a00de9328c0103bc145106ace7dc
Aaron Melcher <amelch130@gmail.com>
2018-10-11 08:51:33 -0500

    Updated Room Activity

--- src/objectOrientedProgramming/Room.java
+++ src/objectOrientedProgramming/Room.java
@@ -1,5 +1,38 @@
 package objectOrientedProgramming;
 
-public class Room {
+import java.util.Scanner;
 
+public class Room {
+	public Room()
+	{
+		
+	}
+	public void area()
+	{
+		Scanner input = new Scanner (System.in);
+		System.out.println("What is the length of the wall (feet)?");
+		int length = input.nextInt();
+		System.out.println("What is the height of the wall (feet)?");
+		int height = input.nextInt();
+		int area = height*length;
+		System.out.println("The area of the wall is "+area+" feet squared");
+		input.close();
+	}
+	public void areaWithDoor()
+	{
+		Scanner input = new Scanner (System.in);
+		System.out.println("What is the length of the wall?");
+		int length = input.nextInt();
+		System.out.println("What is the height of the wall?");
+		int height = input.nextInt();
+		System.out.println("What is the height of the door?");
+		int doorHeight = input.nextInt();
+		System.out.println("What is the width of the door?");
+		int doorWidth = input.nextInt();
+		int area = length*height;
+		int doorArea = doorHeight*doorWidth;
+		area = area - doorArea;
+		System.out.println("The area of the wall without the door is "+area+" feet squared.");
+		input.close();
+	}
 }

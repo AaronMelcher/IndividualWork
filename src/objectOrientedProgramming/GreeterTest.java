8d6523d1eef9445f3f5086b4954fa05bb9a4ea4b
Aaron Melcher <amelch130@gmail.com>
2018-10-16 09:11:48 -0500

    Instance Fields with Greeter Classes

--- src/objectOrientedProgramming/GreeterTest.java
+++ src/objectOrientedProgramming/GreeterTest.java
@@ -5,17 +5,10 @@ public class GreeterTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Greeter bob = new Greeter();
-		bob.sayHello();
-		Friend joe = new Friend();
-		joe.sayMessage();
-		Friend john = new Friend();
-		bob.sayHello();
-		bob.sayHello();
-		bob.sayHello();
-		john.sayMessage();
-		bob.sayHello();
 		
-		
+		bob.setAge(99);
+		int age = bob.getAge();
+		System.out.print(age);
 		
 	}
 

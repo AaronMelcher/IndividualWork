b41ab8f47432fe46dc6eb8c32fc471d6e5416670
Aaron Melcher <amelch130@gmail.com>
2018-10-17 08:37:44 -0500

    Employee Class & Test

---
+++ src/objectOrientedProgramming/EmployeeTest.java
@@ -0,0 +1,14 @@
+package objectOrientedProgramming;
+
+public class EmployeeTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Employee bob = new Employee();
+		bob.setWage(10.25);
+		System.out.println(bob.getWage());
+		bob.setYears(6);
+		System.out.println(bob.getYears());
+	}
+
+}

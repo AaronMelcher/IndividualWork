a7832dc18d0d13893f0c4b08ded7940cc3544b56
Aaron Melcher <amelch130@gmail.com>
2018-10-17 08:19:45 -0500

    Updated Greeter

--- src/objectOrientedProgramming/GreeterTest.java
+++ src/objectOrientedProgramming/GreeterTest.java
@@ -5,11 +5,8 @@ public class GreeterTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Greeter bob = new Greeter();
-		
 		bob.setAge(99);
-		int age = bob.getAge();
-		System.out.print(age);
-		
+		System.out.print(bob.getAge());	
 	}
 
 }

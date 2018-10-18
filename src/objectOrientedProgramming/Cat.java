50d0f178ff2f2dd511a15cab358182cb63d109c0
Aaron Melcher <amelch130@gmail.com>
2018-10-18 08:53:24 -0500

    Cat Update

--- src/objectOrientedProgramming/Cat.java
+++ src/objectOrientedProgramming/Cat.java
@@ -3,28 +3,35 @@ package objectOrientedProgramming;
 public class Cat {
 	private int age;
 	private int sleep;
-	public Cat(){
+
+	public Cat() {
 		age = 0;
 		sleep = 0;
 	}
+
 	public Cat(int newAge, int newSleep) {
 		age = newAge;
 		sleep = newSleep;
 	}
-	public void birthday(){
-		age = age+1;
+
+	public void birthday() {
+		age = age + 1;
 	}
+
 	public void sleepMore(int add) {
-		sleep = sleep+add;
+		sleep = sleep + add;
 	}
+
 	public void sleepLess(int sub) {
-		sleep = sleep-sub;
+		sleep = sleep - sub;
 	}
+
 	public int age() {
 		return age;
 	}
-	public int sleep(){
+
+	public int sleep() {
 		return sleep;
 	}
-	
+
 }

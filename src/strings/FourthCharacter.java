2bc645733693e1a65b849312a57c80a67f15ecb2
Aaron Melcher <amelch130@gmail.com>
2018-11-30 12:18:48 -0600

    String Problems Day 4

---
+++ src/strings/FourthCharacter.java
@@ -0,0 +1,19 @@
+package strings;
+
+import java.util.Scanner;
+
+public class FourthCharacter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter a sentence:");
+		String bob = input.nextLine();
+		int length = bob.length();
+		for (int j = 0; j <= length - 1; j += 4) {
+			System.out.println(bob.charAt(j));
+		}
+		input.close();
+	}
+
+}

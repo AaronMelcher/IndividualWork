3c8d8d9d7b5bddcc7faf13a7bc7c89193ae57224
Aaron Melcher <amelch130@gmail.com>
2018-12-19 09:01:52 -0600

    More MDA stuff

--- src/arrays/MDAFriends.java
+++ src/arrays/MDAFriends.java
@@ -1,24 +1,27 @@
 package arrays;
 
+import java.util.Random;
 import java.util.Scanner;
 
-public class StringMultiDimensional {
+public class MDAFriends {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Random bob = new Random();
 		Scanner input = new Scanner(System.in);
-		final int ROW = 4;
-		final int COL = 3;
-		String[][] names = new String[ROW][COL];
+		final int ROW = 18;
+		final int COL = 5;
+		int max = 100;
+		int min = 0;
+		Friend[][] age = new Friend[ROW][COL];
 		for (int i = 0; i < ROW; i++) {
 			for (int k = 0; k < COL; k++) {
-				System.out.println("Enter a name:");
-				names[i][k] = input.nextLine();
+				age[i][k] = new Friend("a" ,min + bob.nextInt(max - min + 1));
 			}
 		}
 		for (int i = 0; i < ROW; i++) {
 			for (int k = 0; k < COL; k++) {
-				System.out.print(names[i][k].substring(names[i][k].indexOf(" ") + 1) + " ");
+				System.out.print(age[i][k].getAge() + " ");
 			}
 			System.out.println(" ");
 		}

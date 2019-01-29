fb8147461adac5c2d4687b0a23ae42baa8b43dd4
Aaron Melcher <amelch130@gmail.com>
2019-01-29 09:02:20 -0600

    Book Class & Test and Library Class

--- src/designingClasses/Book.java
+++ src/designingClasses/Book.java
@@ -1,5 +1,24 @@
 package designingClasses;
 
 public class Book {
+	private String title;
+	private int pages;
 
+	public Book() {
+		title = "yeet";
+		pages = 69;
+	}
+
+	public Book(String xTitle, int xPages) {
+		title = xTitle;
+		pages = xPages;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public int getPages() {
+		return pages;
+	}
 }

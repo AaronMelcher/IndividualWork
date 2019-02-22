630e6c270673f1c0884873ff94970b86f4de78d6
Aaron Melcher <amelch130@gmail.com>
2019-02-22 08:15:24 -0600

    Finished double searching method

--- src/numberStuff/BinaryClassDecending.java
+++ src/numberStuff/BinaryClassDecending.java
@@ -3,44 +3,52 @@ package numberStuff;
 public class BinaryClassDecending {
 	public static class JavaBinary {
 
-		public static int binarySearchAscending(int [] temp, int value){
+		public static int binarySearchAscending(int[] temp, int value) {
 			int low = 0;
 			int high = temp.length - 1;
-			while (low <= high){
+			while (low <= high) {
 				int mid = (low + high) / 2;
-				if(temp[mid] == value)
+				if (temp[mid] == value)
 					return mid;
+				else if (temp[mid] < value)
+					low = mid + 1;
 				else
-					if(temp[mid] < value)
-						low = mid + 1;
-					else
-						high = mid - 1;
+					high = mid - 1;
 			}
 			return -1;
 		}
-		
-		public static int binarySearchDescending(int [] temp, int value){
+
+		public static int binarySearchDescending(int[] temp, int value) {
 			int low = 0;
 			int high = temp.length - 1;
-			while (low <= high){
+			while (low <= high) {
 				int mid = (low + high) / 2;
-				if(temp[mid] == value)
+				if (temp[mid] == value)
 					return mid;
+				else if (temp[mid] > value)
+					low = mid + 1;
 				else
-					if(temp[mid] > value)
-						low = mid + 1;
-					else
-						high = mid - 1;
+					high = mid - 1;
 			}
 			return -1;
 		}
-		
-		public static double doubleSearchAsceding(double [] temp, double value) {
+
+		public static double doubleSearchAsceding(double[] temp, double value) {
 			int low = 0;
 			int high = temp.length - 1;
-			while(low <= high) {
-				
+			while (low <= high) {
+				int mid = (low + high) / 2;
+				if (temp[mid] - value < 0.00001) {
+					return mid;
+				} else {
+					if (temp[mid] - value > 0.00001) {
+						low = mid + 1;
+					} else {
+						high = mid - 1;
+					}
+				}
 			}
+			return -1;
 		}
 	}
 }

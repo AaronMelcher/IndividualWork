8fd28b32bb792c0006bfba2780df6cb9ff6f86b2
Aaron Melcher <amelch130@gmail.com>
2019-02-27 11:22:13 -0600

    Print Writer class  test

---
+++ src/mtProjecct/PrintWriterTest.java
@@ -0,0 +1,26 @@
+package mtProjecct;
+
+import java.io.*;
+import java.util.*;
+
+public class PrintWriterTest {
+
+	public static void main(String[] args) throws IOException {
+		Scanner input2 = new Scanner(System.in);
+		File gamer = new File("D:/File.IO Test/gamerTime.txt");
+		File gamer2 = new File("D:/File.IO Test/gamerTime.txt");
+		Scanner input = new Scanner(gamer);
+		PrintWriter output = new PrintWriter(gamer2);
+		System.out.println("Type something");
+		String test = input.nextLine();
+		output.print(test);
+		output.print("Hello");
+		output.print(" ");
+		output.println("Hello World");
+		input.close();
+		output.close();
+		input2.close();
+
+	}
+
+}

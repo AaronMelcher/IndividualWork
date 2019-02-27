c8f725ce93b0b9bf25c06b24b6279aaf96dd9a45
Aaron Melcher <amelch130@gmail.com>
2019-02-27 09:36:24 -0600

    File IO Testing

--- src/mtProjecct/FileIOStart.java
+++ src/mtProjecct/FileIOStart.java
@@ -1,5 +1,24 @@
 package mtProjecct;
 
-public class FileIOStart {
+import java.util.*;
+import java.io.*;
 
+public class FileIOStart {
+	public static void main(String[] args) throws IOException {
+		File gamer = new File("D:/File.IO Test/gamerTime.txt");
+		File gamer2 = new File("D:/File.IO Test/gamerTime.txt");
+		Scanner input =  new Scanner(gamer);
+		Scanner input2 = new Scanner(gamer2);
+		while (input.hasNext())
+		{
+			System.out.println(input.nextLine());
+		}
+
+		while (input2.hasNext()){
+			System.out.println(input2.next());
+		}
+
+		input.close();
+		input2.close();
+	}
 }

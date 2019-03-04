8752cae316b244ebb557505ac4bd2b086c3c8c6b
Aaron Melcher <amelch130@gmail.com>
2019-03-04 07:11:16 -0600

    Resume Class and Stuff

--- src/mtProjecct/ResumeClass.java
+++ src/mtProjecct/ResumeClass.java
@@ -6,12 +6,30 @@ import java.util.*;
 public class ResumeClass {
 	private String word;
 	private int count;
-	private File resume;
+	private static File resume = new File("D:/File.IO Test/testDoc.txt");;
 	private Scanner check;
 
 	public ResumeClass() throws IOException {
 		count = 0;
-		resume = new File("D:/File.IO Test/testDoc.txt");
 		check = new Scanner(resume);
+		word = new String();
+	}
+
+	public ResumeClass(String xWord) throws IOException {
+		count = 0;
+		check = new Scanner(resume);
+		word = xWord;
+	}
+
+	public void search() {
+		if(check.hasNext(word) == true) {
+			count++;
+		}
+	}
+	public int getNum() {
+		return count;
+	}
+	public String toString() {
+		return check.nextLine();
 	}
 }
---
+++ src/mtProjecct/ResumeTest.java
@@ -0,0 +1,12 @@
+package mtProjecct;
+import java.io.*;
+public class ResumeTest {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		ResumeClass bob = new ResumeClass("see");
+		bob.search();
+		System.out.println(bob.getNum());
+	}
+}
+

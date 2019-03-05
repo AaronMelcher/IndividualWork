5652ef2636cd1602cc27faafacbe18a8993bfeec
Aaron Melcher <amelch130@gmail.com>
2019-03-05 15:59:19 -0600

    More updates to Resume class stuff

--- src/mtProjecct/ResumeClass.java
+++ src/mtProjecct/ResumeClass.java
@@ -31,6 +31,10 @@ public class ResumeClass {
 		}
 	}
 
+	public String getWord() {
+		return word;
+	}
+
 	public int getNum() {
 		return count;
 	}
--- src/mtProjecct/ResumeTest.java
+++ src/mtProjecct/ResumeTest.java
@@ -1,12 +1,27 @@
 package mtProjecct;
+
 import java.io.*;
+import java.util.*;
+
 public class ResumeTest {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		ResumeClass bob = new ResumeClass("is");
-		bob.search();
-		System.out.println(bob.getNum());
+		ArrayList<ResumeClass> searching = new ArrayList<ResumeClass>();
+		Scanner input = new Scanner(System.in);
+		System.out.println("How many words do you want to search for?");
+		int num = input.nextInt();
+		input.next();
+		int i = 0;
+		while (i < num) {
+			System.out.println("Enter your word:");
+			String bob = input.nextLine();
+			searching.add(new ResumeClass(bob));
+			i++;
+		}
+		for (ResumeClass temp : searching) {
+			System.out.println(temp.getNum() + " appears " + temp.getNum() + " times");
+		}
+		input.close();
 	}
 }
-

0a88c41910cc5bc6681a0c0004152e2742e88002
Aaron Melcher <amelch130@gmail.com>
2019-05-09 09:30:03 -0500

    Cont of Elevens Lab

--- src/elevensLab/DeckTester.java
+++ src/elevensLab/DeckTester.java
@@ -7,11 +7,12 @@ public class DeckTester {
 	 * @param args is not used.
 	 */
 	public static void main(String[] args) {
-		String[] ranks = {"A", "B", "C"};
-		String[] suits = {"Giraffes", "Lions"};
-		int[] values = {2, 1, 6};
+		String[] suits = { "Spades", "Hearts", "Diamonds", "Clubs" };
+		String[] ranks = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace" };
+		int[] values = { 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 10, 11 };
+		Shuffler.selectionShuffle(values);
 		Deck bob = new Deck(ranks, suits, values);
 		System.out.println(bob.toString());
-		
+
 	}
 }
